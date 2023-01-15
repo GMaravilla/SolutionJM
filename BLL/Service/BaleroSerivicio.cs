acee6816c66eb6e496f13500f3dda75c7aa2e533
javier_maravilla <gmaravillaolivar@gmail.com>
2023-01-15 08:49:07 -0600

    agregando la interfaz y clase de servicios en BLL

---
+++ BLL/Service/BaleroSerivicio.cs
@@ -0,0 +1,47 @@
+﻿using Data.DataContext;
+using Data.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Service
+{
+    public class BaleroSerivicio : IBaleroService
+    {
+        private readonly IRepositorioGenerico<Balero> _BaleroRepo;
+
+        public BaleroSerivicio(IRepositorioGenerico<Balero> BaleroRepo)
+        {
+            _BaleroRepo= BaleroRepo;
+        }
+
+        public async Task<bool> Actualizar(Balero model)
+        {
+            return await _BaleroRepo.Actualizar(model);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await _BaleroRepo.Eliminar(id);
+        }
+
+        public async Task<bool> Insertar(Balero model)
+        {
+            return await _BaleroRepo.Insertar(model);
+        }
+
+        public async Task<Balero> Obtener(int id)
+        {
+           return await _BaleroRepo.Obtener(id);
+        }
+
+        public async Task<IQueryable<Balero>> ObtenerTodos()
+        {
+            return await _BaleroRepo.ObtenerTodos();
+        }
+
+        //falta el del datatable
+    }
+}
---
+++ BLL/Service/IBaleroService.cs
@@ -0,0 +1,21 @@
+﻿using Data.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Service
+{
+    public interface IBaleroService
+    {
+        Task<bool> Insertar(Balero model);
+        Task<bool> Actualizar(Balero model);
+        Task<bool> Eliminar(int id);
+        Task<Balero> Obtener(int id);
+        Task<IQueryable<Balero>> ObtenerTodos();
+
+            //falta el task para el datatable
+
+    }
+}
