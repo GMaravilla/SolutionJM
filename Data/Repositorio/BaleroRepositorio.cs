05320db053e29cc05eca8a6fe7d13440654e9484
javier_maravilla <gmaravillaolivar@gmail.com>
2023-01-15 08:36:56 -0600

    proyecto base con configuracion de la capa data

---
+++ Data/Repositorio/BaleroRepositorio.cs
@@ -0,0 +1,52 @@
+﻿using Data.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositorio
+{
+    public class BaleroRepositorio : IRepositorioGenerico<Balero>
+    {
+        private readonly MvcPruebasContext _MvcContext;
+        public BaleroRepositorio(MvcPruebasContext MvcContext) 
+        {
+            _MvcContext = MvcContext;
+        }
+
+        public async Task<bool> Actualizar(Balero model)
+        {
+            _MvcContext.Baleros.Add(model);
+            await _MvcContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+           _MvcContext.Baleros.First(e => e.IdBaleros == id);
+            await _MvcContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Insertar(Balero model)
+        {
+            _MvcContext.Baleros.Add(model);
+            await _MvcContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Balero> Obtener(int id)
+        {
+            return await _MvcContext.Baleros.FindAsync(id);
+        }
+
+        public async Task<IQueryable<Balero>> ObtenerTodos()
+        {
+           IQueryable<Balero> query = _MvcContext.Baleros;
+            return query;
+        }
+
+        //faltaria el del datatable
+    }
+}
---
+++ Data/Repositorio/IRepositorioGenerico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositorio
+{
+    public interface IRepositorioGenerico<TEntityModel> where TEntityModel : class
+    {
+
+        //metodos a utilizar
+        Task<bool> Insertar(TEntityModel model);
+        Task<bool> Actualizar(TEntityModel model);
+        Task<bool> Eliminar(int id);
+        Task<TEntityModel> Obtener(int id);
+        Task<IQueryable<TEntityModel>> ObtenerTodos();
+
+        //falta el llenado de tabla del datatable
+    }
+}
