33d5b15ff8a5adc3376ff3d6724c62349fbd649b
javier_maravilla <gmaravillaolivar@gmail.com>
2023-01-15 10:07:12 -0600

    configuracion de toda la capa del mvc webapp

--- WebAppMVC/Controllers/HomeController.cs
+++ WebAppMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Service;
+using Data.DataContext;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebAppMVC.Models;
 
@@ -6,16 +8,61 @@ namespace WebAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IBaleroService _baleroService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IBaleroService baleroService)
         {
-            _logger = logger;
+            _baleroService= baleroService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var Baleros = await _baleroService.ObtenerTodos();
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insertar([Bind("IdBaleros,Marca,Codigo,Precio,Fecha_Create")]Balero balero)
+        {
+            if(ModelState.IsValid)
+            {
+                bool respuesta = await _baleroService.Insertar(balero);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Hubo un error al insertar el balero en la base de datos";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Actualizar([Bind("IdBaleros,Marca,Codigo,Precio,Fecha_Create")] Balero balero)
+        {
+            if(ModelState.IsValid)
+            {
+                bool respuesta = await _baleroService.Actualizar(balero);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessae = "Hubo un error al actualizar el balero en la base de datos";
+                return View();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            bool respuesta = await _baleroService.Eliminar(id);
+            return StatusCode(StatusCodes.Status200OK, new {valor = respuesta});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Obtener(int id)
+        {
+            Balero respuesta = await _baleroService.Obtener(id);
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
         }
 
         public IActionResult Privacy()
--- WebAppMVC/Program.cs
+++ WebAppMVC/Program.cs
@@ -1,7 +1,22 @@
+using BLL.Service;
+using Data.DataContext;
+using Data.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//agregar el servicio de conexion al datacontext
+builder.Services.AddDbContext<MvcPruebasContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL"));
+});
+
+//agregando los servicios hacia los metodos de la interfaz
+builder.Services.AddScoped<IRepositorioGenerico<Balero>, BaleroRepositorio>();
+builder.Services.AddScoped<IBaleroService, BaleroSerivicio>();
 
 var app = builder.Build();
 
